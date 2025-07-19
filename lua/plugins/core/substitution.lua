5f8fc42a1d56cbf42bec1382b448813b2d5d4075
Giorgio Zonta <giorgio99zonta@outlook.com>
2025-07-19 21:10:50 +0200

    use M-x instead of more mappings

--- lua/plugins/core/substitution.lua
+++ lua/plugins/core/substitution.lua
@@ -28,32 +28,19 @@ return {
   },
 
   {
-    -- filter by open buffers (would be nice if you use % or ## in Path [total Vim integration...])
-    --:lua require('grug-far').open({ prefills = { paths = vim.fn.expand("##") } })
-    -- :lua require('grug-far').open({ prefills = { search = vim.fn.expand("<cword>") } })
 
-    -- non funge bene con autochdir
     'MagicDuck/grug-far.nvim',
-    keys = {
-      -- nice idea because they don't conflict with vim if you use <leader> as prefix also <leader>{<[c,m]-.>} is very uncommon
-      -- would be nice if you can gd on vim commands to go to their (plugin) definition
-      -- also mappings for current cd and workspace? use consistent mappings for those
-      -- think of <space><modifiers> like if <space> is universal argument in emacs, basically just a slightly different behaviour (uppercase version for project)
-      {mode="x",'<space><m-c-s-5>',":GrugFarWithin ast-grep<cr>"},
-      {mode="n",'<space><m-c-s-5>',"<cmd>GrugFar ast-grep<cr>"},
-      {mode="x",'<m-c-s-5>',":GrugFarWithin ripgrep<cr>"},
-      {mode="n",'<m-c-s-5>',"<cmd>GrugFar ripgrep<cr>"},
-      {
-        mode = { 'n', 'x' },
-        '<c-s-/>',
-        function()
-          local search = vim.fn.getreg('/')
-          if search and vim.startswith(search, '\\<') and vim.endswith(search, '\\>') then search = '\\b' .. search:sub(3, -3) .. '\\b' end
-          require('grug-far').open({ prefills = { search = search } })
-        end,
-          { desc = 'grug-far: Search using @/ register value or visual selection' },
-        }
-    },
+    -- keys = {
+    --   {
+    --     mode = { 'n', 'x' }, '<c-s-/>',
+    --     function()
+    --       local search = vim.fn.getreg('/')
+    --       if search and vim.startswith(search, '\\<') and vim.endswith(search, '\\>') then search = '\\b' .. search:sub(3, -3) .. '\\b' end
+    --       require('grug-far').open({ prefills = { search = search } })
+    --     end,
+    --       { desc = 'grug-far: Search using @/ register value or visual selection' },
+    --     }
+    -- },
     -- TODO: turn quickfix into grug-far buffer?
     opts = {
       headerMaxWidth = 80,
@@ -80,18 +67,5 @@ return {
     },
   },
     cmd = 'GrugFar',
-    -- you can always modify the argument list btw (replace w/ open buffers for ex...)
-    -- keys = {{'<c-s-q>',function() require('grug-far').open({ prefills = { paths = vim.fn.expand("##") } }) end}},
-    -- keys = {{mode={'n','x'},'<c-s>',function() require('grug-far').open({ prefills = { paths = vim.fn.expand("##") } }) end}},
-    -- or you could write a function listing open buffers inside paths=...
-    -- keys = {{
-    --   mode={'n','x'},
-    --   -- '<c-s-q>',
-    --   '<c-s>',
-    --   -- function() require('grug-far').open({ prefills = { paths = vim.fn.expand("##") } }) end,
-    --   function() require('grug-far').open({}) end,
-    -- }},
-    --:lua 
-    -- by default, in visual mode, the visual selection is used to prefill the search
   },
 }
