f5e32743dc5b62ace062e862ae089451f08b00b3
Giorgio Zonta <giorgio99zonta@outlook.com>
2025-07-19 18:05:54 +0200

    more robust mapping

--- lua/plugins/core/build.lua
+++ lua/plugins/core/build.lua
@@ -7,14 +7,17 @@ return {
   opts = {},
   keys = {
     {
+      mode = { "i", "n" },
       '<c-c><c-c>',
       function()
-        require('compiler.utils')
-          .require_language(vim.bo.filetype)
-          .action('option' .. vim.v.count1)
+        if vim.bo.filetype == "" then
+          vim.notify("Buffer has no filetype", vim.log.levels.ERROR)
+          return
+        end
+        if not Snacks.git.get_root() then vim.cmd.lcd(vim.fn.expand("%:h")) end
+        require('compiler.utils').require_language(vim.bo.filetype).action('option' .. vim.v.count1)
       end,
       desc = 'Open compiler',
-      ft = { 'asm', 'c', 'cpp', 'cs', 'dart', 'elixir', 'fortran', 'fsharp', 'gleam', 'go', 'java', 'javascript', 'kotlin', 'lua', 'make', 'perl', 'python', 'r', 'ruby', 'rust', 'sh', 'swift', 'typescript', 'vb', 'zig' },
     },
   },
 }
