c66fe88e3fd380d0c114eaba2d29453aef9955bf
Giorgio Zonta <giorgio99zonta@outlook.com>
2025-07-19 21:11:16 +0200

    get rid of useless prefix

--- lua/plugins/snacks/bufdelete.lua
+++ lua/plugins/snacks/bufdelete.lua
@@ -4,10 +4,9 @@ return {
     bufdelete = {},
   },
   keys = {
-    { '<leader>bd', function() Snacks.bufdelete() end, desc = 'Delete Buffer' },
-    { '<leader><bs>', function() Snacks.bufdelete() end, desc = 'Delete Buffer' },
-    { '<leader>bD', function() Snacks.bufdelete({ force = true }) end, desc = 'Delete Buffer (Force)' },
-    { '<leader>bo', function() Snacks.bufdelete.other() end, desc = 'Delete Other Buffers' },
-    { '<leader>bO', function() Snacks.bufdelete.other({ force = true }) end, desc = 'Delete Other Buffers (Force)' },
+    { '<f12>k', function() Snacks.bufdelete() end, desc = 'Delete Buffer' },
+    -- { '<leader>bD', function() Snacks.bufdelete({ force = true }) end, desc = 'Delete Buffer (Force)' },
+    -- { '<leader>bo', function() Snacks.bufdelete.other() end, desc = 'Delete Other Buffers' },
+    -- { '<leader>bO', function() Snacks.bufdelete.other({ force = true }) end, desc = 'Delete Other Buffers (Force)' },
   },
 }
