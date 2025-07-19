38fcc53d9d34ad20bfc45ab8d23747a304e6bfa2
Giorgio Zonta <giorgio99zonta@outlook.com>
2025-07-19 18:10:54 +0200

    fix

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,14 +1,17 @@
--- https://www.reddit.com/r/neovim/comments/1jkdzlm/whats_new_in_neovim_011/
-local configs = {}
-
-for _, v in ipairs(vim.api.nvim_get_runtime_file('lsp/*', true)) do
-  local name = vim.fn.fnamemodify(v, ':t:r')
-  -- TODO: remove in lspconfig 3.0
-  if name ~= 'volar' and vim.fn.exepath(v) then
-  configs[name] = true
-end
-end
-
-vim.lsp.enable(vim.tbl_keys(configs))
-
--- crea comando per ottnere lspconfig per lsp? parse html webpage
+-- -- https://www.reddit.com/r/neovim/comments/1jkdzlm/whats_new_in_neovim_011/
+-- local configs = {}
+--
+-- for _, v in ipairs(vim.api.nvim_get_runtime_file('lsp/*', true)) do
+--   local name = vim.fn.fnamemodify(v, ':t:r')
+--   -- TODO: remove in lspconfig 3.0
+--   if name ~= 'volar' and vim.fn.exepath(v) then
+--   configs[name] = true
+-- end
+-- end
+--
+-- vim.lsp.enable(vim.tbl_keys(configs))
+--
+-- -- crea comando per ottnere lspconfig per lsp? parse html webpage
+--
+vim.lsp.enable('lua_ls')
+vim.lsp.enable('jdtls')
