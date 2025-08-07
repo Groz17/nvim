5502d5166eab7749438f1ed0c5bb2d9307f4a369
Giorgio Zonta <giorgio99zonta@outlook.com>
2025-08-07 04:01:18 +0200

    update aider conf

--- lua/plugins/AI/aider.lua
+++ lua/plugins/AI/aider.lua
@@ -1,25 +1,25 @@
--- https://github.com/milanglacier/minuet-ai.nvim
--- https://www.reddit.com/r/neovim/comments/1gypmjt/from_emacs_to_nvimaider_my_first_lua_plugin/
--- https://github.com/Davidyz/VectorCode
+-- -- https://github.com/milanglacier/minuet-ai.nvim
+-- -- https://www.reddit.com/r/neovim/comments/1gypmjt/from_emacs_to_nvimaider_my_first_lua_plugin/
+-- -- https://github.com/Davidyz/VectorCode
 return {
-  'GeorgesAlkhouri/nvim-aider',
-  cmd = { 'AiderTerminalToggle', 'AiderHealth' },
-  keys = {
-    { '<leader>A/', '<cmd>AiderTerminalToggle<cr>',    desc = 'Open Aider' },
-    { '<leader>As', '<cmd>AiderTerminalSend<cr>',      desc = 'Send to Aider', mode = { 'n', 'x' } },
-    { '<leader>Ac', '<cmd>AiderQuickSendCommand<cr>',  desc = 'Send Command To Aider' },
-    { '<leader>Ab', '<cmd>AiderQuickSendBuffer<cr>',   desc = 'Send Buffer To Aider' },
-    { '<leader>A+', '<cmd>AiderQuickAddFile<cr>',      desc = 'Add File to Aider' },
-    { '<leader>A-', '<cmd>AiderQuickDropFile<cr>',     desc = 'Drop File from Aider' },
-    { '<leader>Ar', '<cmd>AiderQuickReadOnlyFile<cr>', desc = 'Add File as Read-Only' },
-    -- Example nvim-tree.lua integration if needed
-    -- { "<leader>a+", "<cmd>AiderTreeAddFile<cr>",    desc = "Add File from Tree to Aider", ft = "NvimTree" },
-    -- { "<leader>a-", "<cmd>AiderTreeDropFile<cr>",   desc = "Drop File from Tree from Aider", ft = "NvimTree" },
-  },
-  dependencies = {
-    'folke/snacks.nvim',
-    --- The below dependencies are optional
-    -- mini.files???
-  },
-  config = true,
-}
+    "GeorgesAlkhouri/nvim-aider",
+    cmd = "Aider",
+    -- Example key mappings for common actions:
+    keys = {
+      { "<leader>A/", "<cmd>Aider toggle<cr>", desc = "Toggle Aider" },
+      { "<leader>As", "<cmd>Aider send<cr>", desc = "Send to Aider", mode = { "n", "v" } },
+      { "<leader>Ac", "<cmd>Aider command<cr>", desc = "Aider Commands" },
+      { "<leader>Ab", "<cmd>Aider buffer<cr>", desc = "Send Buffer" },
+      { "<leader>A+", "<cmd>Aider add<cr>", desc = "Add File" },
+      { "<leader>A-", "<cmd>Aider drop<cr>", desc = "Drop File" },
+      { "<leader>Ar", "<cmd>Aider add readonly<cr>", desc = "Add Read-Only" },
+      { "<leader>AR", "<cmd>Aider reset<cr>", desc = "Reset Session" },
+      -- Example nvim-tree.lua integration if needed
+      { "<leader>A+", "<cmd>AiderTreeAddFile<cr>", desc = "Add File from Tree to Aider", ft = "NvimTree" },
+      { "<leader>A-", "<cmd>AiderTreeDropFile<cr>", desc = "Drop File from Tree from Aider", ft = "NvimTree" },
+    },
+    dependencies = {
+      "folke/snacks.nvim",
+      },
+   opts = { auto_reload = false }
+  }
