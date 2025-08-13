f56d87ad53ff2c6f5edbad56aa05a67860122cee
Giorgio Zonta <giorgio99zonta@outlook.com>
2025-08-14 01:00:11 +0200

    shorter desc

--- lua/plugins/coding/screenshot.lua
+++ lua/plugins/coding/screenshot.lua
@@ -10,18 +10,18 @@ return{
   -- build = "make build_generator",
   keys = {
     -- add pr to use v:register (add option use_clipboard = true, or smth to set it as default)
-    { "<leader>cc", "<esc><cmd>CodeSnap<CR>", mode = "x", desc = "Save selected code snapshot into clipboard" },
+    { "<leader>cc", "<esc><cmd>CodeSnap<CR>", mode = "x", desc = "Clipboard" },
     -- TODO: how to reference opts.save_path here?
     -- { "<leader>cs", function() require("codesnap").save_snapshot() end , mode = "x", desc = "Save selected code snapshot in ~/Pictures/Code" },
-    { "<leader>cC", "<esc><cmd>CodeSnapSave<CR>", mode = "x", desc = "Save selected code snapshot on the disk" },
+    { "<leader>cC", "<esc><cmd>CodeSnapSave<CR>", mode = "x", desc = "Disk" },
     -- add pr to use v:register (add option use_clipboard or smth to set it as default)
     -- provide save option as well? (magari in different dir...)
-    { "<leader>ca", "<esc><cmd>CodeSnapASCII<CR>", mode = "x", desc = "Take a code snapshot in ASCII format" },
+    { "<leader>ca", "<esc><cmd>CodeSnapASCII<CR>", mode = "x", desc = "ASCII - clipboard" },
     -- TODO: how to customize highlighting???
-    { "<leader>ch", "<esc><cmd>CodeSnapHighlight<CR>", mode = "x", desc = "Take highlighted code blocks into clipboard" },
-    { "<leader>cH", "<esc><cmd>CodeSnapSaveHighlight<CR>", mode = "x", desc = "Save highlighted code blocks on the disk" },
-    {mode="x","<leader>cl",":CodeSnap<space>",desc='Copy to clipboard specifying language extension'},
-    {mode="x","<leader>cL",":CodeSnapSave<space>", desc = "Save to disk specifying language extension"}
+    { "<leader>ch", "<esc><cmd>CodeSnapHighlight<CR>", mode = "x", desc = "Highlight - clipboard" },
+    { "<leader>cH", "<esc><cmd>CodeSnapSaveHighlight<CR>", mode = "x", desc = "Highlight - disk" },
+    {mode="x","<leader>cl",":CodeSnap<space>",desc='Language extension - clipboard '},
+    {mode="x","<leader>cL",":CodeSnapSave<space>", desc = "Language extension - disk "}
   },
   opts = {
     save_path = "~/Pictures/Code",
