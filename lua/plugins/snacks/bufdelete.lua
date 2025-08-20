1d7c554351ae227f5363c04380ab3cf7fbc79bbc
Giorgio Zonta <giorgio99zonta@outlook.com>
2025-08-21 01:45:09 +0200

    reflect emacs mapping

--- lua/plugins/snacks/bufdelete.lua
+++ lua/plugins/snacks/bufdelete.lua
@@ -1,10 +1,11 @@
+-- windelete?
 return {
   'snacks.nvim',
   opts = {
     bufdelete = {},
   },
   keys = {
-    { '<f12>k', function() Snacks.bufdelete() end, desc = 'Delete Buffer' },
+    { '<f12>k<cr>', function() Snacks.bufdelete() end, desc = 'Delete Buffer' },
     -- { '<leader>bD', function() Snacks.bufdelete({ force = true }) end, desc = 'Delete Buffer (Force)' },
     -- { '<leader>bo', function() Snacks.bufdelete.other() end, desc = 'Delete Other Buffers' },
     -- { '<leader>bO', function() Snacks.bufdelete.other({ force = true }) end, desc = 'Delete Other Buffers (Force)' },
