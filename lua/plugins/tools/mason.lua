5c431a44616163e85b719015de13f5e66d886c9a
Giorgio Zonta <giorgio99zonta@outlook.com>
2025-08-21 01:46:06 +0200

    format

--- lua/plugins/tools/mason.lua
+++ lua/plugins/tools/mason.lua
@@ -1,37 +1,38 @@
 return
- -- TODO: disable fidget spinner at the bottom?
- {
-   -- what about scheme?, for treesitter
-   'williamboman/mason.nvim',
-   cmd = {
-     'Mason',
-     'MasonUpdate',
-     'MasonInstall',
-     'MasonUninstall',
-     'MasonUninstallAll',
-     'MasonLog',
-   },
-   -- Lazy-loading the plugin, or somehow deferring the setup, is not recommended.
-   lazy = false,
-   build = ':MasonUpdate', -- :MasonUpdate updates registry contents
-   -- opts = {
-   --   -- border = "single",
-   --   -- winhighlight = "Normal:Normal,FloatBorder:VertSplit,CursorLine:CursorLine,Search:Search",
-   -- },
-   opts = {
-     -- upgrade java 21
-     -- registries = {
-     --   "github:nvim-java/mason-registry"
-     -- },
-     ensure_installed = {
-       'tinymist',
-       'html-lsp',
--- 'harper'
--- "html-lsp", "basedpyright", "bash-debug-adapter", "bash-language-server", "biome", "clangd", "css-lsp", "harper-ls", "java-debug-adapter", "java-test", "jdtls", "lua-language-server", "perlnavigator", "rust-analyzer", "shfmt", "sqlls", "taplo", "vim-language-server",
-     },
-   },
+-- TODO: disable fidget spinner at the bottom?
+{
+  -- what about scheme?, for treesitter
+  'williamboman/mason.nvim',
+  cmd = {
+    'Mason',
+    'MasonUpdate',
+    'MasonInstall',
+    'MasonUninstall',
+    'MasonUninstallAll',
+    'MasonLog',
+  },
+  -- Lazy-loading the plugin, or somehow deferring the setup, is not recommended.
+  lazy = false,
+  build = ':MasonUpdate',  -- :MasonUpdate updates registry contents
+  -- opts = {
+  --   -- border = "single",
+  --   -- winhighlight = "Normal:Normal,FloatBorder:VertSplit,CursorLine:CursorLine,Search:Search",
+  -- },
+  opts = {
+    -- upgrade java 21
+    -- registries = {
+    --   "github:nvim-java/mason-registry"
+    -- },
+    ensure_installed = { -- divide by categories like lsp, etc...?
+      'tinymist',
+      'html-lsp',
+      'docker',
+      -- 'harper'
+      -- "html-lsp", "basedpyright", "bash-debug-adapter", "bash-language-server", "biome", "clangd", "css-lsp", "harper-ls", "java-debug-adapter", "java-test", "jdtls", "lua-language-server", "perlnavigator", "rust-analyzer", "shfmt", "sqlls", "taplo", "vim-language-server",
+    },
+  },
 
-   -- vim.api.nvim_create_user_command("MasonInstallAll", function()
-   --   vim.cmd("MasonInstall " .. table.concat(options.ensure_installed, " "))
-   -- end, {})
- }
+  -- vim.api.nvim_create_user_command("MasonInstallAll", function()
+  --   vim.cmd("MasonInstall " .. table.concat(options.ensure_installed, " "))
+  -- end, {})
+}
