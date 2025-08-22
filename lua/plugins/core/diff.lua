624ab3117cd3fd1694d8a522a392c997708d69c2
Giorgio Zonta <giorgio99zonta@outlook.com>
2025-08-22 06:44:44 +0200

    simplify config

--- lua/plugins/core/diff.lua
+++ lua/plugins/core/diff.lua
@@ -35,21 +35,17 @@ return {
       -- ft = {'git','diff'}, ??? global ft plxxx
       -- {],[}{d,D} have to with macros, so we can disable them in diff filetypes. nevermind, extend unimpaired mappings
       -- It kinda works also with ft=git for Git ++curwin diff or git -p diff (fugitive)
-      { mode = { 'n', 'x', 'o' }, ']d', '<Plug>(textobj-diff-file-n)', ft = { 'git', 'diff' }, desc = 'Move to the beginning of the next header of files' },
-      { mode = { 'n', 'x', 'o' }, '[d', '<Plug>(textobj-diff-file-p)', ft = { 'git', 'diff' }, desc = 'Move to the beginning of the previous header of files' },
-      { mode = { 'n', 'x', 'o' }, ']D', '<Plug>(textobj-diff-file-N)', ft = { 'git', 'diff' }, desc = 'Move to the end of the next header of files' },
-      { mode = { 'n', 'x', 'o' }, '[D', '<Plug>(textobj-diff-file-P)', ft = { 'git', 'diff' }, desc = 'Move to the end of the previous header of files' },
-      { mode = { 'n', 'x', 'o' }, ']h', '<Plug>(textobj-diff-hunk-n)', ft = { 'git', 'diff' }, desc = 'Move to the beginning of the next hunk' },
-      { mode = { 'n', 'x', 'o' }, '[h', '<Plug>(textobj-diff-hunk-p)', ft = { 'git', 'diff' }, desc = 'Move to the beginning of the previous hunk' },
-      { mode = { 'n', 'x', 'o' }, ']H', '<Plug>(textobj-diff-hunk-N)', ft = { 'git', 'diff' }, desc = 'Move to the end of the next hunk' },
-      { mode = { 'n', 'x', 'o' }, '[H', '<Plug>(textobj-diff-hunk-P)', ft = { 'git', 'diff' }, desc = 'Move to the end of the previous hunk' },
-      -- no diagnostic/@conditional here :p
-      -- make these work with mini.ai (mini.ai should have an api for external text objects) for next/last
-      { mode = { 'x', 'o' }, 'ad', '<Plug>(textobj-diff-file)', ft = { 'git', 'diff' }, desc = 'Select all hunks and the header of the next/previous files' },
-      { mode = { 'x', 'o' }, 'id', '<Plug>(textobj-diff-file)', ft = { 'git', 'diff' }, desc = 'Select all hunks and the header of the next/previous files' },
+      { mode = { 'n', 'x', 'o' }, '<m-n>', '<Plug>(textobj-diff-hunk-n)', ft = { 'git', 'diff' }, desc = 'Move to the beginning of the next hunk' },
+      { mode = { 'n', 'x', 'o' }, '<m-p>', '<Plug>(textobj-diff-hunk-p)', ft = { 'git', 'diff' }, desc = 'Move to the beginning of the previous hunk' },
+      { mode = { 'n', 'x', 'o' }, '<m-s-n>', '<Plug>(textobj-diff-file-n)', ft = { 'git', 'diff' }, desc = 'Move to the beginning of the next header of files' },
+      { mode = { 'n', 'x', 'o' }, '<m-s-p>', '<Plug>(textobj-diff-file-p)', ft = { 'git', 'diff' }, desc = 'Move to the beginning of the previous header of files' },
+
       -- would be cool if this ignored the initial header (should start from @@ line) (to include them use ad and id to exclude trailing whitespace)
       { mode = { 'x', 'o' }, 'ah', '<Plug>(textobj-diff-file)', ft = { 'git', 'diff' }, desc = 'Select all hunks and the header of the next/previous files' },
       { mode = { 'x', 'o' }, 'ih', '<Plug>(textobj-diff-hunk)', ft = { 'git', 'diff' }, desc = 'Select the next/previous hunk' },
+      -- make these work with mini.ai (mini.ai should have an api for external text objects) for next/last
+      { mode = { 'x', 'o' }, 'ad', '<Plug>(textobj-diff-file)', ft = { 'git', 'diff' }, desc = 'Select all hunks and the header of the next/previous files' },
+      { mode = { 'x', 'o' }, 'id', '<Plug>(textobj-diff-file)', ft = { 'git', 'diff' }, desc = 'Select all hunks and the header of the next/previous files' },
     },
   },
 
