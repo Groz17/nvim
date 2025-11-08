5fa56030c4f022629aa2951bd53a83b5701f7ab0
Giorgio Zonta <giorgio99zonta@outlook.com>
2025-11-08 20:08:56 +0100

    don't need all that

--- lua/plugins/colorschemes/themes.lua
+++ lua/plugins/colorschemes/themes.lua
@@ -15,31 +15,5 @@ return {
       vim.cmd [[colorscheme ef-theme]]
     end,
   },
-  --         ╭──────────────────────────────────────────────────────────╮
-  --         │              COLORSCHEME-AFFECTING PLUGINS               │
-  --         ╰──────────────────────────────────────────────────────────╯
-  -- https://github.com/skywind3000/vim-color-patch
-  {
-    "xiyaowong/transparent.nvim",
-    cmd = { 'TransparentToggle', 'TransparentEnable', 'TransparentDisable' },
-    keys = { { '<f13>0', '<cmd>TransparentToggle<cr>' }, },
-    opts = {
-      extra_groups = {
-        "NormalFloat",    -- plugins which have float panel such as Lazy, Mason, LspInfo
-        "NvimTreeNormal", -- NvimTree
-      },
-      -- lualine_style = "default",
-      -- lualine_style = "stealth",
-    },
-    config = function()
-      local transparent = require("transparent")
-      transparent.clear_prefix("lualine")
-      -- if config.transparent then
-      -- vim.cmd("TransparentEnable")
-      -- end
-    end,
-  },
-
-
 
 }
