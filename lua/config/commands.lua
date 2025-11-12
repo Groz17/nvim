cc60b4333179453858e265f6e1bb64df904dc055
Giorgio Zonta <giorgio99zonta@outlook.com>
2025-11-13 00:18:19 +0100

    Add DirDiff command

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -122,3 +122,20 @@ wincmd n
 pu=random_subset
 ]]
 , { desc = 'Test Emacs config...' })
+
+-- https://www.reddit.com/r/neovim/comments/1ov1gtr/difftool_wrapper/
+vim.api.nvim_create_user_command('DirDiff', function(opts)
+  if vim.tbl_count(opts.fargs) ~= 2 then
+    vim.notify('DirDiff requires exactly two directory arguments', vim.log.levels.ERROR)
+    return
+  end
+
+  vim.cmd 'tabnew'
+  vim.cmd.packadd 'nvim.difftool'
+  require('difftool').open(opts.fargs[1], opts.fargs[2], {
+    rename = {
+      detect = false,
+    },
+    ignore = { '.git' },
+  })
+end, { complete = 'dir', nargs = '*' })
