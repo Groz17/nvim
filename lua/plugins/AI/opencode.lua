32102d015f1a1078fb017c2b9c19a57562935684
Giorgio Zonta <giorgio99zonta@outlook.com>
2025-11-23 23:23:20 +0100

    add paste images from clipboard mapping

--- lua/plugins/AI/opencode.lua
+++ lua/plugins/AI/opencode.lua
@@ -9,8 +9,9 @@ return {
     keymap = {
       input_window = {
         ['<c-p>'] = { 'prev_prompt_history', mode = { 'i' } }, -- Navigate to previous prompt in history
-        ['<c-n>'] = { 'next_prompt_history', mode = { 'i' } }, --
+        ['<c-n>'] = { 'next_prompt_history', mode = { 'i' } }, -- Navigate to next prompt in history
         ['<tab>'] = { 'switch_mode', mode = { 'n', 'i' } }, -- Switch between modes (build/plan)
+        ['<c-s-v>'] = { 'paste_image', mode = { 'n', 'i' } }, -- Paste image from the clipboard
       },
     },
   },
