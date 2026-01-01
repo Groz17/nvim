f7fa05706c8cf9ff28b9ab56565dd2fc5e35f39a
Giorgio Zonta <giorgio99zonta@outlook.com>
2026-01-01 23:37:01 +0100

    https://github.com/esmuellert/vscode-diff.nvim/issues/97

--- lua/plugins/git/neogit.lua
+++ lua/plugins/git/neogit.lua
@@ -3,7 +3,7 @@ return {
   "NeogitOrg/neogit",
   dependencies = {
     "nvim-lua/plenary.nvim",  -- required
-    "sindrets/diffview.nvim", -- optional - Diff integration
+    -- TODO: "sindrets/diffview.nvim", -- optional - Diff integration
 
     "folke/snacks.nvim",      -- optional
   },
