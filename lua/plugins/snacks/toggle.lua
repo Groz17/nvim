c74503f40456bb7052b215a202778178fbe12686
Giorgio Zonta <giorgio99zonta@outlook.com>
2026-08-03 17:36:39 +0200

    why not toggle in visual mode too?

--- lua/plugins/snacks/toggle.lua
+++ lua/plugins/snacks/toggle.lua
@@ -11,51 +11,51 @@ return {
 
         -- basically unbounded vim/emacs keymaps could become kanata's modifiers
         -- what about visual mode? just operate on that region
-        -- Snacks.toggle.animate():map("<f13>a", { mode = {"n","i"} })
-        -- Snacks.toggle.line_number():map("<f13>n", { mode = {"n","i"} })
-        Snacks.toggle.option("number"):map("<f13>n", { mode = {"n","i"} })
+        -- Snacks.toggle.animate():map("<f13>a", { mode = {"n","i","x"} })
+        -- Snacks.toggle.line_number():map("<f13>n", { mode = {"n","i","x"} })
+        Snacks.toggle.option("number"):map("<f13>n", { mode = {"n","i","x"} })
         -- maybe use uppercase for lsp-related toggle mappings?
-        -- Snacks.toggle.diagnostics():map([[\D]], { mode = {"n","i"} })
-        Snacks.toggle.diagnostics():map("<f13>d", { mode = {"n","i"} })
-        -- Snacks.toggle.treesitter():map([[\T]], { mode = {"n","i"} })
-        Snacks.toggle.treesitter():map("<f13>t", { mode = {"n","i"} })
+        -- Snacks.toggle.diagnostics():map([[\D]], { mode = {"n","i","x"} })
+        Snacks.toggle.diagnostics():map("<f13>d", { mode = {"n","i","x"} })
+        -- Snacks.toggle.treesitter():map([[\T]], { mode = {"n","i","x"} })
+        Snacks.toggle.treesitter():map("<f13>t", { mode = {"n","i","x"} })
         -- H?
-        Snacks.toggle.inlay_hints():map("<f13>h", { mode = {"n","i"} })
-        -- Snacks.toggle.scroll():map("<f13>j", { mode = {"n","i"} })
-        Snacks.toggle.indent():map("<f13>i", { mode = {"n","i"} })
-        Snacks.toggle.dim():map("<f13>D", { mode = {"n","i"} })
-        -- Snacks.toggle.zoom():map("<f13>o", { mode = {"n","i"} }) -- like ^wo vim mapping
-        Snacks.toggle.zoom():map("<f12>1", { mode = {"n","i"} }) -- like win+f WM fullscreen mapping
-        Snacks.toggle.zen():map("<f13>z", { mode = {"n","i"} })
+        Snacks.toggle.inlay_hints():map("<f13>h", { mode = {"n","i","x"} })
+        -- Snacks.toggle.scroll():map("<f13>j", { mode = {"n","i","x"} })
+        Snacks.toggle.indent():map("<f13>i", { mode = {"n","i","x"} })
+        Snacks.toggle.dim():map("<f13>D", { mode = {"n","i","x"} })
+        -- Snacks.toggle.zoom():map("<f13>o", { mode = {"n","i","x"} }) -- like ^wo vim mapping
+        Snacks.toggle.zoom():map("<f12>1", { mode = {"n","i","x"} }) -- like win+f WM fullscreen mapping
+        Snacks.toggle.zen():map("<f13>z", { mode = {"n","i","x"} })
 
-        Snacks.toggle.words():map("<f13>R", { mode = {"n","i"} })
+        Snacks.toggle.words():map("<f13>R", { mode = {"n","i","x"} })
         -- ╭─────────────────────────────────────────────────────────╮
         -- │ options                                                 │
         -- ╰─────────────────────────────────────────────────────────╯
-        Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<f13>b", { mode = {"n","i"} })
-        Snacks.toggle.option("cursorline", {}):map("<f13>c", { mode = {"n","i"} })
+        Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<f13>b", { mode = {"n","i","x"} })
+        Snacks.toggle.option("cursorline", {}):map("<f13>c", { mode = {"n","i","x"} })
         -- *[od*	*]od*	*yod*	'diff' (actually |:diffthis| / |:diffoff|)
-        -- Snacks.toggle.option("diff", {}):map("<f13>d", { mode = {"n","i"} })
-        -- Snacks.toggle.option("hlsearch", {}):map("<f13>d", { mode = {"n","i"} })
-        -- Snacks.toggle.option("ignorecase", {}):map("<f13>i", { mode = {"n","i"} })
-        Snacks.toggle.option("list", { name = "List" }):map("<f13>l", { mode = {"n","i"} })
-        -- Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<f13>r", { mode = {"n","i"} })
-        Snacks.toggle.option("spell", { name = "Spelling" }):map("<f13>s", {mode={"n","i"}})
-        -- Snacks.toggle.option("colorcolumn", { name = "Spelling" }):map("<f13>s", {mode={"n","i"}})
-        -- Snacks.toggle.option("cursorcolumn", { name = "Spelling" }):map("<f13>s", {mode={"n","i"}})
+        -- Snacks.toggle.option("diff", {}):map("<f13>d", { mode = {"n","i","x"} })
+        -- Snacks.toggle.option("hlsearch", {}):map("<f13>d", { mode = {"n","i","x"} })
+        -- Snacks.toggle.option("ignorecase", {}):map("<f13>i", { mode = {"n","i","x"} })
+        Snacks.toggle.option("list", { name = "List" }):map("<f13>l", { mode = {"n","i","x"} })
+        -- Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<f13>r", { mode = {"n","i","x"} })
+        Snacks.toggle.option("spell", { name = "Spelling" }):map("<f13>s", {mode={"n","i","x"}})
+        -- Snacks.toggle.option("colorcolumn", { name = "Spelling" }):map("<f13>s", {mode={"n","i","x"}})
+        -- Snacks.toggle.option("cursorcolumn", { name = "Spelling" }):map("<f13>s", {mode={"n","i","x"}})
         -- TODO:  Fixa
-        -- Snacks.toggle.option("virtualedit", { name = "Virtualedit" }):map("<f13>v", { mode = {"n","i"} })
-        Snacks.toggle.option("wrap", { name = "Wrap" }):map("<f13>w", { mode = {"n","i"} })
-        Snacks.toggle.option("wrap", { name = "Wrap" }):map("<f12>xt", { mode = {"n","i"} }) -- toggle-truncate-lines
-        Snacks.toggle.option("modifiable", { name = "Modifiable" }):map("<f13>m", { mode = {"n","i"} })
-        Snacks.toggle.option("modified", { name = "Modified" }):map("<m-s-`>", { mode = {"n","i"} })
+        -- Snacks.toggle.option("virtualedit", { name = "Virtualedit" }):map("<f13>v", { mode = {"n","i","x"} })
+        Snacks.toggle.option("wrap", { name = "Wrap" }):map("<f13>w", { mode = {"n","i","x"} })
+        Snacks.toggle.option("wrap", { name = "Wrap" }):map("<f12>xt", { mode = {"n","i","x"} }) -- toggle-truncate-lines
+        Snacks.toggle.option("modifiable", { name = "Modifiable" }):map("<f13>m", { mode = {"n","i","x"} })
+        Snacks.toggle.option("modified", { name = "Modified" }):map("<m-s-`>", { mode = {"n","i","x"} })
 
-        Snacks.toggle.option("modifiable", { name = "Modifiable" }):map("<f12><c-q>", { mode = {"n","i"} })
+        Snacks.toggle.option("modifiable", { name = "Modifiable" }):map("<f12><c-q>", { mode = {"n","i","x"} })
         -- like emacs c-x c-q
-        -- Snacks.toggle.option("readonly", { name = "Readonly" }):map("<f12><c-q>", { mode = {"n","i"} })
+        -- Snacks.toggle.option("readonly", { name = "Readonly" }):map("<f12><c-q>", { mode = {"n","i","x"} })
         -- yoL?
         -- yoc?
-        Snacks.toggle.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map("<f13>e", { mode = {"n","i"} })
+        Snacks.toggle.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map("<f13>e", { mode = {"n","i","x"} })
         -- yol?
       end,
     })
